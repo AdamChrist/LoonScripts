dbeb3f2e18842689f72d76526bdff3af23ed801c
AdamChrist <haojiachen1988@126.com>
2025-10-10 12:35:04 +0800

    更新配置

--- js/hellsquadrrrush.js
+++ js/hellsquadrrrush.js
@@ -1,7 +1,7 @@
 const body = JSON.parse($response.body);
 
 if (body.hasOwnProperty('IsPlayableAd')) {
-    body.IsPlayableAd = false;
+    body.IsPlayableAd = true;
 }
 
 $done({body: JSON.stringify(body)});
